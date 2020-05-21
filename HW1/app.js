3a8a8f57ef55ca9907da71f6c89b95dee9270412
homecomp <0966009853m@gmail.com>
2020-05-21 18:11:01 +0300

    hw1 updata

--- HW1/app.js
+++ HW1/app.js
@@ -1,3 +1,20 @@
+const path = require('path');
+const fs =require('fs').promises
+
+//jsonplece
+const fetch = require('axios')
+const api = 'https://jsonplaceholder.typicode.com/users'
+const fileName = 'text.json'
+fetch(api)
+    .then(({data}) => fs.writeFile(fileName, JSON.stringify(data)))
+    .then(()=>fs.readFile(fileName))
+    .then(usersFromFile =>{
+     // const userJSON = JSON.parse(usersFromFile.toString())
+     //    console.log(userJSON[0].address.geo);
+        console.log(JSON.parse(usersFromFile.toString()));
+    })
+    .catch(err => console.log(err))
+
 // const path = require('path');
 // const fs =require('fs').promises;
 //
@@ -27,53 +44,53 @@
 // swap('1800','2000');
 
 //вариант 2 ;
-const path = require('path');
-const fs =require('fs')
-
-function swap(folderFrom,folderTo) {
-   const directoryFrom = path.join(__dirname, folderFrom );
-   const directoryTo = path.join(__dirname, folderTo );
-   const directorySwapFolder =  path.join(__dirname,'swapfolder')
-
-   createDirectory(directorySwapFolder)
-       .then(()=> copyFile(directoryFrom, directorySwapFolder))
-       .then(()=> copyFile(directoryTo,directoryFrom ))
-       .then(()=> copyFile(directorySwapFolder,directoryTo))
-       .then(()=> deleteDirectory(directorySwapFolder))
-       .catch(err => console.log(err));
-}
-
-const createDirectory = (directorySwapFolder)=> new Promise((resolve, reject)=>{
-     fs.mkdir(directorySwapFolder, err => {
-        if (err) reject(err);
-        resolve();
-     })
-  });
-
-const deleteDirectory = (directorySwapFolder)=> new Promise((resolve, reject)=>{
-    fs.rmdir(directorySwapFolder, err => {
-        if (err) reject(err);
-        resolve();
-    })
-});
-
-const copyFile = (directoryFrom, directoryTo) => new Promise ((resolve, reject)=>
-    {fs.readdir(directoryFrom,(err,files)=>{
-            if(err) console.log(err);
-            resolve(files);
-        }) ;
-    }).then(files => new Promise((resolve, reject)=>{
-    for (const file of files){
-        fs.rename(
-            path.join(directoryFrom, file),
-            path.join(directoryTo, file),
-            err => {
-                if (err) reject(err)
-            }
-      )
-   }
-    resolve();
-}));
-
-
-swap('1800','2000')
+// const path = require('path');
+// const fs =require('fs')
+//
+// function swap(folderFrom,folderTo) {
+//    const directoryFrom = path.join(__dirname, folderFrom );
+//    const directoryTo = path.join(__dirname, folderTo );
+//    const directorySwapFolder =  path.join(__dirname,'swapfolder')
+//
+//    createDirectory(directorySwapFolder)
+//        .then(()=> copyFile(directoryFrom, directorySwapFolder))
+//        .then(()=> copyFile(directoryTo,directoryFrom ))
+//        .then(()=> copyFile(directorySwapFolder,directoryTo))
+//        .then(()=> deleteDirectory(directorySwapFolder))
+//        .catch(err => console.log(err));
+// }
+//
+// const createDirectory = (directorySwapFolder)=> new Promise((resolve, reject)=>{
+//      fs.mkdir(directorySwapFolder, err => {
+//         if (err) reject(err);
+//         resolve();
+//      })
+//   });
+//
+// const deleteDirectory = (directorySwapFolder)=> new Promise((resolve, reject)=>{
+//     fs.rmdir(directorySwapFolder, err => {
+//         if (err) reject(err);
+//         resolve();
+//     })
+// });
+//
+// const copyFile = (directoryFrom, directoryTo) => new Promise ((resolve, reject)=>
+//     {fs.readdir(directoryFrom,(err,files)=>{
+//             if(err) console.log(err);
+//             resolve(files);
+//         }) ;
+//     }).then(files => new Promise((resolve, reject)=>{
+//     for (const file of files){
+//         fs.rename(
+//             path.join(directoryFrom, file),
+//             path.join(directoryTo, file),
+//             err => {
+//                 if (err) reject(err)
+//             }
+//       )
+//    }
+//     resolve();
+// }));
+//
+//
+// swap('1800','2000')
